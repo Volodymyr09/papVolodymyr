7d1fe2d822a9a8a14b60cf113576e865d656d82f
Volodymyr09 <jekvorobey151@gmail.com>
2021-03-05 16:03:18 +0000

    Admin; Includes;

---
+++ admin/ListaChave.php
@@ -0,0 +1,18 @@
+<?php
+include_once ("includes/body.inc.php");
+top();
+?>
+
+
+    <div class="container">
+        <h1>Chaves</h1>
+
+        <div id="tableContent">
+
+        </div>
+    </div>
+
+
+<?php
+    Bottom();
+?>
---
+++ admin/includes/body.inc.php
@@ -0,0 +1,85 @@
+<?php
+
+include_once("config.inc.php");
+    $con=mysqli_connect(HOST,USER,PWD,DATABASE);
+    $con->set_charset("utf8");
+function top()
+{
+    ?>
+
+    <!DOCTYPE HTML>
+    <html>
+    <head>
+        <title>PC-GAMER</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/main.css" />
+
+        <link rel="icon" href="images/log.jpg">
+        <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+
+    </head>
+    <body class="is-preload">
+
+
+    <!-- Wrapper -->
+    <body id="wrapper">
+
+        <!-- Header -->
+        <header id="header" class="alt">
+
+            <nav>
+                <a  href="#menu">Menu</a>
+            </nav>
+
+
+        </header>
+
+
+
+        <!-- Menu -->
+        <nav id="menu">
+
+
+            <ul class="links">
+                <li class="active"> <a href="#">Categorias</a> </li>
+
+                <li> <a href="#">Produtos</a> </li>
+
+                <li> <a href="ListaChave.php">Chaves</a> </li>
+
+            </ul>
+        </nav>
+
+
+    <?php
+}
+
+    function Bottom(){
+    ?>
+        <!-- Footer -->
+        <footer id="footer">
+            <div class="inner">
+                <ul class="copyright">
+                    <li>PC-GAMER PAP © 2021</li>
+                    <li> <a href="https://www.phpjabbers.com/">PHPJabbers.com</a></li>
+                </ul>
+            </div>
+        </footer>
+
+        <!-- Scripts -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/jquery.scrolly.min.js"></script>
+        <script src="assets/js/jquery.scrollex.min.js"></script>
+        <script src="assets/js/browser.min.js"></script>
+        <script src="assets/js/breakpoints.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <script src="assets/js/main.js"></script>
+        </body>
+    </HTML>
+
+<?php
+}
+?>
