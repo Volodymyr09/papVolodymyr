89111e902ec94a8029b77b3a3abb50ee1b556db2
Volodymyr09 <jekvorobey151@gmail.com>
2021-03-23 17:13:02 +0000

    Pagina Principal: Produtos; Crud: Noticia;

--- admin/confirmaNovaNoticia.php
+++ admin/confirmaNovaNoticia.php
@@ -6,13 +6,13 @@ $nome=addslashes($_POST['tituloNoticia']);
 $link=addslashes($_POST['linkNoticia']);
 $imagem=$_FILES['imagemNoticia']['name'];
 $novoNome="../images/".$imagem;
-
+$destaque=addslashes($_POST['destaqueProduto']);
 
 
 copy($_FILES['imagemNoticia']['tmp_name'],$novoNome);
 
-echo $sql="insert into noticias(noticiaTitulo, noticiaLinkURL, noticiaImagemURL)
-values('".$nome."','".$link."','images/".$imagem."')";
+echo $sql="insert into noticias(noticiaTitulo, noticiaLinkURL, noticiaImagemURL, noticiaDestaque)
+values('".$nome."','".$link."','images/".$imagem."',".$destaque.")";
 mysqli_query($con,$sql);
 header("location:listaNoticia.php");
 ?>
