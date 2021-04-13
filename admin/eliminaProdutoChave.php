d1c8ee12d3b29995300db9eee6033bab809363ce
Volodymyr09 <jekvorobey151@gmail.com>
2021-04-13 16:52:35 +0100

    Crud: ProdutoChave: Eliminar;

--- admin/eliminaProdutoChave.php
+++ admin/eliminaProdutoChave.php
@@ -1,7 +1,7 @@
 <?php
 include_once ("includes/body.inc.php");
 $id=intval($_GET['id']);
-$produto=intval($_POST['produtoChaveProduto']);
+
 
 $sql= "delete from produtochaves where produtoChaveChaveId=".$id;
 mysqli_query($con,$sql);
